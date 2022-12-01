0e4f73c2394a5fd797910fbc40d9fcf3062b0b87
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-12-01 16:22:25 +0700

    Convert indentation to tabs

--- importer.gd
+++ importer.gd
@@ -6,50 +6,50 @@ enum Presets {}
 
 
 func get_importer_name() -> String:
-    return "atirut.flac"
+	return "atirut.flac"
 
 
 func get_visible_name() -> String:
-    return "FLAC Audio"
+	return "FLAC Audio"
 
 
 func get_recognized_extensions() -> Array:
-    return ["flac"]
+	return ["flac"]
 
 
 func get_save_extension() -> String:
-    return "sample"
+	return "sample"
 
 
 func get_resource_type() -> String:
-    return "AudioStreamSample"
+	return "AudioStreamSample"
 
 
 func get_preset_count() -> int:
-    return Presets.size()
+	return Presets.size()
 
 
 func get_preset_name(preset: int) -> String:
-    match preset:
-        _:
-            return "Unknown"
+	match preset:
+		_:
+			return "Unknown"
 
 
 func get_import_options(preset: int) -> Array:
-    match preset:
-        _:
-            return []
+	match preset:
+		_:
+			return []
 
 
 func get_option_visibility(option: String, options: Dictionary) -> bool:
-    return true
+	return true
 
 
 func import(source_file: String, save_path: String, options: Dictionary, platform_variants: Array, gen_files: Array) -> int:
-    var file := File.new()
-    assert(file.open(source_file, File.READ) == OK, "failed to open %s" % source_file)
-    assert(file.get_buffer(4).get_string_from_ascii() == "fLaC", "not an FLAC stream")
-    
-    var stream := AudioStreamSample.new()
+	var file := File.new()
+	assert(file.open(source_file, File.READ) == OK, "failed to open %s" % source_file)
+	assert(file.get_buffer(4).get_string_from_ascii() == "fLaC", "not a FLAC stream")
+	
+	var stream := AudioStreamSample.new()
 
-    return ResourceSaver.save("%s.%s" % [save_path, get_save_extension()], stream)
+	return ResourceSaver.save("%s.%s" % [save_path, get_save_extension()], stream)
