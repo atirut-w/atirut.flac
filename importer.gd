7a4c2c3254f6bf468158be62659ff13aaba8fbb1
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-12-01 15:05:17 +0700

    Importer stub

---
+++ importer.gd
@@ -0,0 +1,51 @@
+tool
+extends EditorImportPlugin
+
+
+enum Presets {}
+
+
+func get_importer_name() -> String:
+    return "atirut.flac"
+
+
+func get_visible_name() -> String:
+    return "FLAC Audio"
+
+
+func get_recognized_extensions() -> Array:
+    return ["flac"]
+
+
+func get_save_extension() -> String:
+    return "sample"
+
+
+func get_resource_type() -> String:
+    return "AudioStreamSample"
+
+
+func get_preset_count() -> int:
+    return Presets.size()
+
+
+func get_preset_name(preset: int) -> String:
+    match preset:
+        _:
+            return "Unknown"
+
+
+func get_import_options(preset: int) -> Array:
+    match preset:
+        _:
+            return []
+
+
+func get_option_visibility(option: String, options: Dictionary) -> bool:
+    return true
+
+
+func import(source_file: String, save_path: String, options: Dictionary, platform_variants: Array, gen_files: Array) -> int:
+    var stream := AudioStreamSample.new()
+
+    return ResourceSaver.save("%s.%s" % [save_path, get_save_extension()], stream)
--- plugin.gd
+++ plugin.gd
@@ -2,9 +2,13 @@ tool
 extends EditorPlugin
 
 
+var importer: EditorImportPlugin
+
+
 func _enter_tree():
-	pass
+	importer = preload("res://addons/atirut.flac/importer.gd").new()
+	add_import_plugin(importer)
 
 
 func _exit_tree():
-	pass
+	remove_import_plugin(importer)
