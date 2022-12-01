8491980be483ef0e7400c81ae5f73f0b506e39f2
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-12-01 15:06:22 +0700

    Make `plugin.gd` type-safe

--- plugin.gd
+++ plugin.gd
@@ -5,10 +5,10 @@ extends EditorPlugin
 var importer: EditorImportPlugin
 
 
-func _enter_tree():
+func _enter_tree() -> void:
 	importer = preload("res://addons/atirut.flac/importer.gd").new()
 	add_import_plugin(importer)
 
 
-func _exit_tree():
+func _exit_tree() -> void:
 	remove_import_plugin(importer)
